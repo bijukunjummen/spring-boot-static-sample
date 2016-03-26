7dac7c43da9e3c9c791727e7f487a1d80352f0cc
Biju Kunjummen <Biju.Kunjummen@gmail.com>
2016-03-25 22:24:26 -0700

    Spring boot SPA sample

---
+++ src/app/index.module.js
@@ -0,0 +1,7 @@
+(function() {
+  'use strict';
+
+  angular
+    .module('springBootStaticSample', ['ngAnimate', 'ngCookies', 'ngTouch', 'ngSanitize', 'ngMessages', 'ngAria', 'ngResource', 'ui.router', 'ui.bootstrap', 'toastr']);
+
+})();
